42edc6b532b74362d9bb724e18a9b5fcac24758c
colenhuttran <tranthenhut@gmail.com>
2021-04-07 11:18:10 -0500

    update good practice

--- angular-directive-pipe/src/app/bigger.directive.ts
+++ angular-directive-pipe/src/app/bigger.directive.ts
@@ -6,19 +6,20 @@ import { Directive, ElementRef, HostListener, Input, OnChanges, Renderer2, Simpl
 export class BiggerDirective {
 
   @Input() makeBigger : number;
-
   constructor(private element: ElementRef,
             private renderer2: Renderer2
     ) {
     this.makeBigger = 2;
-    this.element.nativeElement.style.fontSize = "18px";
+    // this.element.nativeElement.style.fontSize = "18px";
+    this.renderer2.setStyle(this.element.nativeElement, "font-size", "18px");
   }
 
   @HostListener('dblclick', ['$event'])
   onClick(event){
-    let newSize = this.makeBigger + parseInt(this.element.nativeElement.style.fontSize) + "px";    
-    this.element.nativeElement.style.fontSize = newSize;
-    this.element.nativeElement.innerHTML = "Size is " + newSize;
+    let newSize = this.makeBigger + parseInt(this.element.nativeElement.style.fontSize) + "px";
+    // this.element.nativeElement.style.fontSize = newSize;
+    // this.element.nativeElement.innerHTML = "Size is " + newSize;
+    this.renderer2.setStyle(this.element.nativeElement, "font-size", newSize);
   }
 
 
